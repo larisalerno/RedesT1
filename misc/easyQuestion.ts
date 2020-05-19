231f8a8bdbbd85b3a04fd8f8dc22e4c3da36aa46
Larissa Salerno <larissa.salerno@acad.pucrs.br>
2020-05-18 22:41:55 -0300

    Questions and alternatives descriptions

---
+++ misc/easyQuestion.ts
@@ -0,0 +1,93 @@
+import { Question } from "../shared/interfaces/question.interface";
+
+const questions: Question[] = [
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    },
+
+    {
+        description : "Qual é a cor do símbolo da esperança?",
+        alternatives : [
+            'a) Amarelo',
+            'b) Azul',
+            'c) Branco',
+            'd) Verde'
+        ],
+        correctAnwserIndex: 4
+    }
+]
+
+export {questions};
